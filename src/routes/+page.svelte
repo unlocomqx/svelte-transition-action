0a5db27bb68e60f4b25fe48123170616746e477f
unloco <amxmodx.unloco@gmail.com>
2023-04-14 00:10:44 +0100

    Add more examples

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import { transition } from "../lib/transition"
-  import { fade, slide } from "svelte/transition"
+  import { blur, fade, fly, scale, slide } from "svelte/transition"
 
   let displayed = false
 </script>
 
 <button on:click={() => displayed = !displayed}>Toggle</button>
 
-<div use:transition={{fn: fade, key: displayed, duration: 400}}>Toggled content</div>
-<div use:transition={{fn: slide, key: displayed, duration: 400}}>Toggled content</div>
+<div use:transition={{fn: fade, key: displayed, duration: 4000}}>Toggled content</div>
+<div use:transition={{fn: slide, key: displayed, duration: 4000}}>Toggled content</div>
+<div use:transition={{fn: blur, key: displayed, duration: 4000}}>Toggled content</div>
+<div use:transition={{fn: fly, key: displayed, duration: 4000}}>Toggled content</div>
+<div use:transition={{fn: scale, key: displayed, duration: 4000}}>Toggled content</div>
